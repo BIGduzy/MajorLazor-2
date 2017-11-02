4d3f4eb41a22772c956f2a4dbde880d9fa06a3ee
Niels <nielsdwaal@gmail.com>
2017-11-02 12:14:47 +0100

    Setup done for irSignal. Class setup done. No clue why does works tho

--- src/Application-Logics/irSignal.hpp
+++ src/Application-Logics/irSignal.hpp
@@ -1,9 +1,26 @@
-#ifndef IRPROTOCOL_HPP
-#define IRPROTOCOL_HPP
+#ifndef IRSIGNAL_HPP
+#define IRSIGNAL_HPP
 
-class irCommand {
+#include "hwlib.hpp"
+#include "playerTask.hpp"
+
+class irSignal {
+private:
 public:
-    irCommand();
+    irSignal();
+
+    auto encode(uint8_t playerId, uint8_t data) {
+        auto signal = 0;
+        signal |= 1 << 15;
+        signal |= playerId << 10;
+        signal |= data << 5;
+        signal |= playerId ^ data;
+        return signal;
+    }
+
+    void decode(uint16_t message) {
+        setMessage((message >> 10) & 0x1F, (message >> 9) & 0x01, (message >> 5) & 0xF);
+    }
 }
 
-#endif // IRPROTOCOL_HPP
+#endif // IRSIGNAL_HPP
