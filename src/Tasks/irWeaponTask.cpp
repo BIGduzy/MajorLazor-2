5615fd325097e0958a81d768c8d8fa0431144355
Niels <nielsdwaal@gmail.com>
2017-11-07 14:51:43 +0100

    Converted irSignal to the appropriate tasks

--- src/Tasks/irWeaponTask.cpp
+++ src/Tasks/irWeaponTask.cpp
@@ -1,1 +1,36 @@
 #include "irWeaponTask.hpp"
+
+void IrWeaponTask::main() {
+    for (;;) {
+        switch(state) {
+            case IDLE_STATE:
+                break;
+            case SEND_STATE:
+                break;
+        }
+    }
+}
+
+void IrWeaponTask::idleState() {
+    auto evt = wait(fireButtonFlag);
+    state = SEND_STATE;
+}
+
+void IrWeaponTask::sendState() {
+    weaponMutex.wait();
+    auto w = weaponInfoPool.read();
+    irSender.send(irSignal.encode(w.playerId, w.commandId, w.data));
+    weaponMutex.signal();
+    
+    state = IDLE_STATE;
+}
+
+void IrWeaponTask::writeToPool(Message poolMessage) {
+    weaponMutex.wait();
+    weaponInfoPool.write(poolMessage);
+    weaponMutex.signal();
+}
+
+void IrWeaponTask::startShooting() {
+    fireButtonFlag.set();
+}
--- src/Tasks/irWeaponTask.hpp
+++ src/Tasks/irWeaponTask.hpp
@@ -4,15 +4,49 @@
 #include "hwlib.hpp"
 #include "rtos.hpp"
 
-//#include "irSender.hpp"
+#include "irSignal.hpp"
+#include "irSender.hpp"
+#include "message.hpp"
 
 class IrWeaponTask: public rtos::task<> {
+private:
+    rtos::pool<Message> weaponInfoPool;
+    rtos::mutex weaponMutex;
+    rtos::flag fireButtonFlag;
+    enum States {IDLE_STATE, SEND_STATE};
+    States state = IDLE_STATE;
+
+    IrSender irSender;
+    IrSignal irSignal;
+
+    /**
+     * @brief Function representing the idle state.
+     */
+    void idleState();
+    /**
+     * @brief Function representing the sending state.
+     */
+    void sendState();
+
 public:
     IrWeaponTask():
-        task("IrWeaponTask")
+        task("IrWeaponTask"),
+        weaponInfoPool("weaponInfoPool"),
+        weaponMutex("weaponMutex"),
+        fireButtonFlag(this, "fireButtonFlag")
     {}
 
     void main() override;
+
+    /**
+     * @brief Function for writing the current weaponInfo to the pool.
+     * @param poolMessage[in] Message struct containing weaponInfo.
+     */
+    void writeToPool(Message poolMessage);
+    /**
+     * @brief Start shooting using the current weaponInfo.
+     */
+    void startShooting();
 };
 
 #endif // IRWEAPONTASK_HPP
