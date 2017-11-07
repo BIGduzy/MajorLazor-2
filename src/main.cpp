3c45046ca1b6dc16353ed847d54638b3b4f6c63d
BIGduzy <nick-b@live.nl>
2017-11-07 18:16:45 +0100

    Fixed Carlos fuck-up

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 #include <rtos.hpp>
 
 #include "irDetecTask.hpp"
-#include "irSender.hpp"
-#include "playerTask.hpp"
-#include "buttonTask.hpp"
-#include "tmp.hpp"
+#include "gameLeaderTask.hpp"
 #include "oledDisplayTask.hpp"
 #include "irWeaponTask.hpp"
 #include "keypadTask.hpp"
@@ -17,46 +14,36 @@ int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms( 500 );
     
-    IrSender sender(5, 24);
     IrWeaponTask irWeaponTask;
 
     OledDisplayTask oledDisplay;
-    PlayerTask playerTask(oledDisplay);
+    GameLeaderTask gameLeaderTask(irWeaponTask, oledDisplay);
 
-    auto buttonPin = hwlib::target::pin_in(hwlib::target::pins::d7);
-    Button button(buttonPin);
-    ButtonTask buttonTask(irWeaponTask, playerTask, button);
-    // TODO: Remove when gameLeader works
-    TmpTask tmp(playerTask);
+    // auto receiverData   = hwlib::target::pin_in(hwlib::target::pins::d8);
+    // auto receiverGround = hwlib::target::pin_out(hwlib::target::pins::d9);
+    // auto receiverVcc    = hwlib::target::pin_out(hwlib::target::pins::d10);
+    // IrDetecTask IrDetecTask(
+    //    playerTask,
+    //    receiverData,
+    //    receiverGround,
+    //    receiverVcc
+    // );
 
-    auto receiverData   = hwlib::target::pin_in(hwlib::target::pins::d8);
-    auto receiverGround = hwlib::target::pin_out(hwlib::target::pins::d9);
-    auto receiverVcc    = hwlib::target::pin_out(hwlib::target::pins::d10);
-    IrDetecTask IrDetecTask(
-       playerTask,
-       receiverData,
-       receiverGround,
-       receiverVcc
-    );
-    
-    
-    // Make pins for keypad
-    auto row0 = hwlib::target::pin_in(hwlib::target::pins::d46);
-    auto row1 = hwlib::target::pin_in(hwlib::target::pins::d48);
-    auto row2 = hwlib::target::pin_in(hwlib::target::pins::d50);
-    auto row3 = hwlib::target::pin_in(hwlib::target::pins::d52);
-    auto col0 = hwlib::target::pin_oc(hwlib::target::pins::d38);
-    auto col1 = hwlib::target::pin_oc(hwlib::target::pins::d40);
-    auto col2 = hwlib::target::pin_oc(hwlib::target::pins::d42);
-    auto col3 = hwlib::target::pin_oc(hwlib::target::pins::d44);
-    // Make ports for keypad
-    hwlib::port_oc_from_pins colPort = hwlib::port_oc_from_pins(col0, col1, col2, col3);
-    hwlib::port_in_from_pins rowPort = hwlib::port_in_from_pins(row0, row1, row2, row3);
-    // Make matrix for keypad
-    hwlib::matrix_of_switches matrix = hwlib::matrix_of_switches(colPort, rowPort);
-    // Make keypad
-    hwlib::keypad<16> keypad = hwlib::keypad< 16 >(matrix, "123A456B789C*0#D");
-    KeypadTask keypadTask(keypad);
+    auto in0  = hwlib::target::pin_in(hwlib::target::pins::a3);
+	auto in1  = hwlib::target::pin_in(hwlib::target::pins::a2);
+	auto in2  = hwlib::target::pin_in(hwlib::target::pins::a1);
+	auto in3  = hwlib::target::pin_in(hwlib::target::pins::a0);
+	auto out0 = hwlib::target::pin_oc(hwlib::target::pins::a7);
+	auto out1 = hwlib::target::pin_oc(hwlib::target::pins::a6);
+	auto out2 = hwlib::target::pin_oc(hwlib::target::pins::a5);
+    auto out3 = hwlib::target::pin_oc(hwlib::target::pins::a4);
+	// Setup necessary items for the keypad
+	auto outPort = hwlib::port_oc_from_pins(out0, out1, out2, out3);
+	auto inPort = hwlib::port_in_from_pins(in0, in1, in2, in3);
+	auto matrix = hwlib::matrix_of_switches(outPort, inPort);
+	auto keypad = hwlib::keypad<16>(matrix, "123A456B789C*0#D");
+
+    KeypadTask keypadTask(gameLeaderTask, keypad);
 
     // Startup rtos
     rtos::run();
