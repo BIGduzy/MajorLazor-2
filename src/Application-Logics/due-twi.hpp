1f1a18cb91f78f0f978e73890436bf72f929c2b8
BIGduzy <nick-b@live.nl>
2017-11-08 13:47:21 +0100

    No brackets + Nick trying to help = 1.5 hours of time fucking gone

--- src/Application-Logics/due-twi.hpp
+++ src/Application-Logics/due-twi.hpp
@@ -60,8 +60,9 @@ namespace due
 
 			for(size_t i = 0; i < n; ++i) {
 				status = TWI0->TWI_SR;
-				if (status & TWI_SR_NACK)
+				if (status & TWI_SR_NACK) {
 					// hwlib::cout << "status & NACK" << hwlib::endl;
+				}
 				
 				if (status & TWI_SR_TXRDY) {
 					write_byte(*(data + i));
