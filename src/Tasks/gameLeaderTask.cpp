6d66e6657a2d2a5a69b1d775775900ca6d5e3166
BIGduzy <nick-b@live.nl>
2017-11-08 15:45:59 +0100

    GameLeader only sends data when command is ready

--- src/Tasks/gameLeaderTask.cpp
+++ src/Tasks/gameLeaderTask.cpp
@@ -16,26 +16,28 @@ void GameLeaderTask::main() {
         if (evt == commandChannel) {
             auto command = commandChannel.read();
             
-            // Send data to weapon
-            switch(command.commandId) {
-                case 0: // Set time
-                    irWeaponTask.writeToPool(Message(0, 0, command.value));
-                    break;
-                case 1: // Start game
-                    irWeaponTask.writeToPool(Message(0, 0, 0 ));
-                default: // Other commands
-                    // Tell player what field we want to edit
-                    // We do commandId - 2 because the commands start at 0
-                    irWeaponTask.writeToPool(Message(0, 1, command.commandId - 2 ));
-                    irWeaponTask.startShooting();
-                    // Set field value
-                    irWeaponTask.writeToPool(Message(0, 1, command.value ));
-            }
-
-            irWeaponTask.startShooting();
-            
-            // Display to oled
+            // Display command to oled
             display.setDisplay(command);
+
+            // Send data to weapon
+            if (command.send) {
+                switch(command.commandId) {
+                    case 0: // Set time
+                        irWeaponTask.writeToPool(Message(0, 0, command.value));
+                        break;
+                    case 1: // Start game
+                        irWeaponTask.writeToPool(Message(0, 0, 0 ));
+                    default: // Other commands
+                        // Tell player what field we want to edit
+                        // We do commandId - 2 because the commands start at 0
+                        irWeaponTask.writeToPool(Message(0, 1, command.commandId - 2 ));
+                        irWeaponTask.startShooting();
+                        // Set field value
+                        irWeaponTask.writeToPool(Message(0, 1, command.value ));
+                }
+
+                irWeaponTask.startShooting();
+            }
         }
     }
 }
